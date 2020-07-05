81723f97aa088e859b2c161b2e0d2af264faddcf
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-05 21:15:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  p planeteer_calls & valid_calls
+  planeteer_calls & valid_calls
 
 
 
